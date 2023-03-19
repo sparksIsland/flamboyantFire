3c9fb0680e3a6bbe5460f02cb920230e45e62246
sparksIsland <zhangdengzd@foxmail.com>
2023-03-19 16:22:46 +0800

    删除无效文件

--- payment/src/main/java/com/spark/springcloud/user/dao/SignInDao.java
+++ payment/src/main/java/com/spark/springcloud/user/dao/SignInDao.java
@@ -3,8 +3,6 @@ package com.spark.springcloud.user.dao;
 import com.spark.springcloud.user.entities.SignIn;
 import org.apache.ibatis.annotations.Mapper;
 
-import java.util.Date;
-
 @Mapper
 public interface SignInDao {
     SignIn validateLogon(String userName, String passWord);
